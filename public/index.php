0763aad957f3a76413bb12cdbed5d8921a49bee8
zKittyGB <a-hebert@hotmail.fr>
2026-05-05 12:37:28 +0200

    Make SQL dump step non-blocking in CI artifact workflow

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__).'/config/bootstrap_db_env.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
